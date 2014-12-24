b8110f44813481edecd94bf0165ae8b6028ae228
kongo2002 <kongo2002@googlemail.com>
2014-12-24 15:42:34 +0100

    event: handle fork and exit only

--- event.c
+++ event.c
@@ -146,9 +146,7 @@ handle_proc_ev(int nl_sock)
 
         switch (nlcn_msg.proc_ev.what)
         {
-            case PROC_EVENT_NONE:
-                printf("set mcast listen ok\n");
-                break;
+            /* we are interested in fork and exit only */
             case PROC_EVENT_FORK:
                 printf("fork: parent tid=%d pid=%d -> child tid=%d pid=%d\n",
                         nlcn_msg.proc_ev.event_data.fork.parent_pid,
@@ -156,33 +154,18 @@ handle_proc_ev(int nl_sock)
                         nlcn_msg.proc_ev.event_data.fork.child_pid,
                         nlcn_msg.proc_ev.event_data.fork.child_tgid);
                 break;
-            case PROC_EVENT_EXEC:
-                printf("exec: tid=%d pid=%d\n",
-                        nlcn_msg.proc_ev.event_data.exec.process_pid,
-                        nlcn_msg.proc_ev.event_data.exec.process_tgid);
-                break;
-            case PROC_EVENT_UID:
-                printf("uid change: tid=%d pid=%d from %d to %d\n",
-                        nlcn_msg.proc_ev.event_data.id.process_pid,
-                        nlcn_msg.proc_ev.event_data.id.process_tgid,
-                        nlcn_msg.proc_ev.event_data.id.r.ruid,
-                        nlcn_msg.proc_ev.event_data.id.e.euid);
-                break;
-            case PROC_EVENT_GID:
-                printf("gid change: tid=%d pid=%d from %d to %d\n",
-                        nlcn_msg.proc_ev.event_data.id.process_pid,
-                        nlcn_msg.proc_ev.event_data.id.process_tgid,
-                        nlcn_msg.proc_ev.event_data.id.r.rgid,
-                        nlcn_msg.proc_ev.event_data.id.e.egid);
-                break;
             case PROC_EVENT_EXIT:
                 printf("exit: tid=%d pid=%d exit_code=%d\n",
                         nlcn_msg.proc_ev.event_data.exit.process_pid,
                         nlcn_msg.proc_ev.event_data.exit.process_tgid,
                         nlcn_msg.proc_ev.event_data.exit.exit_code);
                 break;
+            case PROC_EVENT_NONE:
+            case PROC_EVENT_EXEC:
+            case PROC_EVENT_UID:
+            case PROC_EVENT_GID:
             default:
-                printf("unhandled proc event\n");
+                /* unhandled events */
                 break;
         }
     }
