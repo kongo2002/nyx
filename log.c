7f8ed9255cc6bf4ca31335060ef1ab9cadc103ea
kongo2002 <kongo2002@googlemail.com>
2014-12-26 23:33:21 +0000

    add log functions

---
+++ log.c
@@ -0,0 +1,105 @@
+#include "log.h"
+
+#define _GNU_SOURCE
+
+#include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+static volatile int quiet = 0;
+
+static const char *
+get_log_color(log_level_e level, size_t *length)
+{
+    const char *color;
+
+    if (level & LOG_INFO)
+        color = "\033[36m";
+    else if (level & LOG_WARN)
+        color = "\033[33m";
+    else if (level & LOG_CRITICAL)
+        color = "\033[31;1m";
+    else if (level & LOG_DEBUG)
+        color = "\033[37m";
+    else if (level & LOG_PERROR)
+        color = "\033[35m";
+    else
+        color = "\033[32m";
+
+    *length = strlen(color);
+
+    return color;
+}
+
+static void
+log_msg(log_level_e level, const char *msg, size_t length)
+{
+    static const size_t end_length = 4;
+    static const char *end_color= "\033[0m";
+
+    /* safe errno */
+    int error = errno;
+
+    size_t start_length;
+    const char *start_color = get_log_color(level, &start_length);
+
+    fwrite(start_color, start_length, 1, stdout);
+    fwrite(msg, length, 1, stdout);
+
+    /* errno specific handling */
+    if (level & LOG_PERROR)
+    {
+        char buffer[512];
+        char *error_msg = strerror_r(error, buffer, 511);
+
+        fputc(':', stdout);
+        fputc(' ', stdout);
+        fwrite(error_msg, strlen(error_msg), 1, stdout);
+    }
+
+    fwrite(end_color, end_length, 1, stdout);
+    fputc('\n', stdout);
+
+    /* restore errno? */
+    /* errno = error; */
+}
+
+static void
+log_format_msg(log_level_e level, const char *format, va_list values)
+{
+    char *msg;
+
+    int length = vasprintf(&msg, format, values);
+
+    if (length > 0)
+    {
+        log_msg(level, msg, length);
+        free(msg);
+    }
+}
+
+#define DECLARE_LOG_FUNC(fn_, level_) \
+    void \
+    log_##fn_(const char *format, ...) \
+    { \
+        if (quiet) return; \
+        va_list vas; \
+        va_start(vas, format); \
+        log_format_msg(level_, format, vas); \
+        va_end(vas); \
+        if ((level_) & LOG_CRITICAL) abort(); \
+    }
+
+DECLARE_LOG_FUNC (debug,    LOG_DEBUG)
+DECLARE_LOG_FUNC (info,     LOG_INFO)
+DECLARE_LOG_FUNC (warn,     LOG_WARN)
+DECLARE_LOG_FUNC (error,    LOG_ERROR)
+DECLARE_LOG_FUNC (perror,   LOG_PERROR)
+DECLARE_LOG_FUNC (critical, LOG_CRITICAL)
+
+#undef DECLARE_LOG_FUNC
+
+/* vim: set et sw=4 sts=4 tw=80: */
---
+++ log.h
@@ -0,0 +1,30 @@
+#ifndef __NYX_LOG_H__
+#define __NYX_LOG_H__
+
+#define _GNU_SOURCE
+
+typedef enum
+{
+    LOG_DEBUG    = 1 << 0,
+    LOG_INFO     = 1 << 1,
+    LOG_WARN     = 1 << 2,
+    LOG_ERROR    = 1 << 3,
+    LOG_PERROR   = 1 << 4,
+    LOG_CRITICAL = 1 << 5
+} log_level_e;
+
+#define DECLARE_LOG_PROTO(type_) \
+void log_##type_(const char* format, ...);
+
+DECLARE_LOG_PROTO (debug)
+DECLARE_LOG_PROTO (info)
+DECLARE_LOG_PROTO (warn)
+DECLARE_LOG_PROTO (error)
+DECLARE_LOG_PROTO (perror)
+DECLARE_LOG_PROTO (critical)
+
+#undef DECLARE_LOG_PROTO
+
+#endif
+
+/* vim: set et sw=4 sts=4 tw=80: */
