37da14138f2971ee34b3d440f8326700dccb0625
kongo2002 <kongo2002@googlemail.com>
2014-12-28 17:02:09 +0000

    add split_string function

---
+++ utils.c
@@ -0,0 +1,69 @@
+#include "list.h"
+#include "utils.h"
+
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+static int
+empty_or_whitespace(const char *str)
+{
+    char c;
+
+    if (str == NULL)
+        return 1;
+
+    while ((c = *str) != '\0')
+    {
+        if (!isspace(c))
+            return 0;
+        str++;
+    }
+
+    return 1;
+}
+
+const char **
+split_string(const char *str)
+{
+    unsigned long i = 0, size = 0;
+    static const char *whitespace = " \t";
+    char *string, *token, *to_free;
+    const char **output;
+    list_node_t *node;
+    list_t *tokens;
+
+    if (str == NULL || *str == '\0')
+        return NULL;
+
+    string = strdup(str);
+    to_free = string;
+    tokens = list_new();
+
+    while ((token = strsep(&string, whitespace)) != NULL)
+    {
+        if (!empty_or_whitespace(token))
+            list_add(tokens, strdup(token));
+    }
+
+    size = list_size(tokens);
+
+    if (size < 1)
+        return NULL;
+
+    output = calloc(size + 1, sizeof(char *));
+
+    node = tokens->head;
+    while (node)
+    {
+        output[i++] = node->data;
+        node = node->next;
+    }
+
+    list_destroy(tokens);
+    free(to_free);
+
+    return output;
+}
+
+/* vim: set et sw=4 sts=4 tw=80: */
---
+++ utils.h
@@ -0,0 +1,9 @@
+#ifndef __NYX_UTILS_H__
+#define __NYX_UTILS_H__
+
+const char **
+split_string(const char *string);
+
+#endif
+
+/* vim: set et sw=4 sts=4 tw=80: */
