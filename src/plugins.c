af92272779ca01be11248c912bd6f63df3bab483
kongo2002 <kongo2002@googlemail.com>
2015-03-15 18:07:31 +0000

    plugins: start implementing discovery logic

---
+++ src/plugins.c
@@ -0,0 +1,72 @@
+/* Copyright 2014-2015 Gregor Uhlenheuer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "def.h"
+#include "log.h"
+#include "plugins.h"
+
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
+
+static const char *
+get_plugin(const char *name)
+{
+    if (name == NULL || *name == '\0')
+        return NULL;
+
+    char *last_dot = strrchr(name, '.');
+
+    if (!last_dot || strncmp(last_dot, ".so", 3))
+        return NULL;
+
+    size_t len = last_dot - name;
+    if (!len)
+        return NULL;
+
+    char *plugin_name = xcalloc(len+1, sizeof(char));
+    strncpy(plugin_name, name, len);
+
+    return plugin_name;
+}
+
+int
+discover_plugins(const char *directory, list_t *plugins)
+{
+    int found = 0;
+
+    if (directory == NULL || *directory == '\0')
+        return found;
+
+    DIR *dir = opendir(directory);
+    if (dir)
+    {
+        struct dirent *entry = NULL;
+        while ((entry = readdir(dir)) != NULL)
+        {
+            const char *plugin_name = get_plugin(entry->d_name);
+            if (!plugin_name)
+                continue;
+
+            log_debug("Found plugin '%s'", plugin_name);
+        }
+
+        closedir(dir);
+    }
+
+    return found;
+}
+
+/* vim: set et sw=4 sts=4 tw=80: */
