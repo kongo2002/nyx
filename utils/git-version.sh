a3205d545766d84fbac908cc1d8c67ab39e2bb69
kongo2002 <kongo2002@googlemail.com>
2016-01-09 17:47:01 +0100

    minor tweak to git-version.sh

--- utils/git-version.sh
+++ utils/git-version.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -d .git -a -r .git ]; then
-    GIT_VERSION=$(git describe --always)
+    GIT_VERSION=$(git describe --always 2>/dev/null)
 
     if [ x"$GIT_VERSION" != x ]; then
         echo "-DGIT_VERSION=\\\"$GIT_VERSION\\\""
