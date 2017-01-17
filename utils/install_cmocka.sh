115686fa52b4d822714f09fce776f066bb04bba9
kongo2002 <kongo2002@googlemail.com>
2017-01-17 21:30:59 +0100

    travis: fix install_cmocka.sh

--- utils/install_cmocka.sh
+++ utils/install_cmocka.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-wget https://cmocka.org/files/1.0/cmocka-1.0.0.tar.xz
+wget --no-check-certificate https://cmocka.org/files/1.0/cmocka-1.0.0.tar.xz
 tar xf cmocka-1.0.0.tar.xz
 cd cmocka-1.0.0
 mkdir build
