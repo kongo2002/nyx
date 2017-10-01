373416cbde14baf4d6059acb2bde4799e74d960f
kongo2002 <kongo2002@googlemail.com>
2017-10-01 17:44:50 +0200

    travis: try clang build with newer cmocka

--- utils/install_cmocka.sh
+++ utils/install_cmocka.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 
-wget --no-check-certificate https://cmocka.org/files/1.0/cmocka-1.0.0.tar.xz
-tar xf cmocka-1.0.0.tar.xz
-cd cmocka-1.0.0
+wget --no-check-certificate https://cmocka.org/files/1.1/cmocka-1.1.1.tar.xz
+tar xf cmocka-1.1.1.tar.xz
+cd cmocka-1.1.1
 mkdir -p build
 cd build
 cmake ..
 make cmocka_shared
 
 cd ../..
-rm -f cmocka-1.0.0.tar.xz
+rm -f cmocka-1.1.1.tar.xz
